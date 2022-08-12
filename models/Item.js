d7dbcae11e84844560c0418ed9bb553693847e85
Bagus Windhu Aponsa <windunc@gmail.com>
2022-08-12 10:10:24 +0700

    feat: init commit

--- models/Item.js
+++ models/Item.js
@@ -2,33 +2,33 @@ const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema;
 
 const itemSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-     country: {
-         type: String,
-         default: 'Indonesia'
-    },
-      city: {
-        type: String,
-        required: true,
-    },
-       isPopular: {
-        type: Boolean,
-    },
-             city: {
-        type: String,
-        required: true,
-    },
-                   city: {
-        type: String,
-        required: true,
-    },
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  country: {
+    type: String,
+    default: "Indonesia",
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  isPopular: {
+    type: Boolean,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('Item', itemSchema);
+module.exports = mongoose.model("Item", itemSchema);
